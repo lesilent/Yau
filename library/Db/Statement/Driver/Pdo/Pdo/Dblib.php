46f8ff888ff3877043af925f780c7c72dca9538d
root <root@development.corp.pmi.edu>
2016-02-26 14:28:47 -0700

    Workaround for bug in https://github.com/php/php-src/pull/1758

--- library/Db/Statement/Driver/Pdo/Pdo/Dblib.php
+++ library/Db/Statement/Driver/Pdo/Pdo/Dblib.php
@@ -23,6 +23,88 @@ class Dblib extends Pdo
 {
 /*=======================================================*/
 
+/**
+* Flag for whether we've already cleared first row set
+*
+* @var  boolean
+* @link https://bugs.php.net/bug.php?id=69592
+*/
+private $next_rowset = FALSE;
+
+/**
+* Return the number of rows affected by the last SQL statement
+*
+* @return integer
+*/
+public function rowCount()
+{
+	$sth = $this->dbh->query('SELECT @@ROWCOUNT');
+	return $sth->fetchColumn();
+}
+
+/**
+* Fetch a single row from result set as an associative array
+*
+* @return mixed a row from the result set, or FALSE if there are no more
+*/
+public function fetchAssocRow()
+{
+	$row = $this->sth->fetch(\PDO::FETCH_ASSOC);
+	if ($row === FALSE && !$this->next_rowset && $this->sth->nextRowset())
+	{
+		$this->next_rowset = TRUE;
+		$row = $this->sth->fetch(\PDO::FETCH_ASSOC);
+	}
+	return $row;
+}
+
+/**
+* Fetch a single row from result set as a numeric array
+*
+* @return mixed a row from the result set, or FALSE if there are no more
+*/
+public function fetchNumRow()
+{
+	$row = $this->sth->fetch(\PDO::FETCH_NUM);
+	if ($row === FALSE && !$this->next_rowset && $this->sth->nextRowset())
+	{
+		$this->next_rowset = TRUE;
+		$row = $this->sth->fetch(\PDO::FETCH_NUM);
+	}
+	return $row;
+}
+
+/**
+* Fetch all results from result set as an array of associative arrays
+*
+* @return array
+*/
+public function fetchAssocAll()
+{
+	$rows = $this->sth->fetchAll(\PDO::FETCH_ASSOC);
+	if (empty($rows) && !$this->next_rowset && $this->sth->nextRowset())
+	{
+		$this->next_rowset = TRUE;
+		$rows = $this->sth->fetchAll(\PDO::FETCH_ASSOC);
+	}
+	return $rows;
+}
+
+/**
+* Fetch all results from result set as an array of numeric arrays
+*
+* @return array
+*/
+public function fetchNumAll()
+{
+	$rows = $this->sth->fetchAll(\PDO::FETCH_NUM);
+	if (empty($rows) && !$this->next_rowset && $this->sth->nextRowset())
+	{
+		$this->next_rowset = TRUE;
+		$rows = $this->seth->fetchAll(\PDO::FETCH_NUM);
+	}
+	return $rows;
+}
 
 /*=======================================================*/
 }
