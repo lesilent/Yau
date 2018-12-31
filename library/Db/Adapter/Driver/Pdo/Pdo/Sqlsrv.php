baacd2b73b3a5ce26183bc219a75be1fe4d3fb9b
John Yau <jyau@pmi.edu>
2018-12-31 14:42:19 -0700

    Add adapter for sqlsrv

---
+++ library/Db/Adapter/Driver/Pdo/Pdo/Sqlsrv.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+* Yau Tools
+*
+* @author   John Yau
+* @category Yau
+* @package  Yau_DB
+*/
+
+namespace Yau\Db\Adapter\Driver\Pdo\Pdo;
+
+use Yau\Db\Adapter\Driver\Pdo\Pdo;
+
+/**
+* Database adapter class for use with SQLServer PDO connection objects
+*
+* @author   John Yau
+* @category Yau
+* @package  Yau_DB
+* @link     http://www.php.net/manual/en/ref.pdo-sqlsrv.php
+*/
+class Sqlsrv extends Pdo
+{
+/*=======================================================*/
+
+
+/*=======================================================*/
+}
