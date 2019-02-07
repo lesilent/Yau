0dc68169566cc99b9b3dbb0336194dec4338afa6
John Yau <jyau@pmi.edu>
2019-02-06 17:04:49 -0700

    Return @@ROWCOUNT when calling exec for pdo_sqlsrv

--- library/Db/Adapter/Driver/Pdo/Pdo/Sqlsrv.php
+++ library/Db/Adapter/Driver/Pdo/Pdo/Sqlsrv.php
@@ -13,7 +13,7 @@ namespace Yau\Db\Adapter\Driver\Pdo\Pdo;
 use Yau\Db\Adapter\Driver\Pdo\Pdo;
 
 /**
-* Database adapter class for use with SQLServer PDO connection objects
+* Database adapter class for use with PDO DBLIB connection objects
 *
 * @author   John Yau
 * @category Yau
@@ -24,6 +24,50 @@ class Sqlsrv extends Pdo
 {
 /*=======================================================*/
 
+/**
+* Execute a statement and return the number of affected rows
+*
+* @param  string  $stmt the SQL statement to execute
+* @param  array   $params optional array of values to bind to query
+* @return integer the number of rows affected, or FALSE on error
+*/
+public function exec($stmt, array $params = array())
+{
+	return (($rowcount = parent::exec($stmt, $params)) < 0
+		&& ($sth = $this->dbh->query('SELECT @@ROWCOUNT')))
+		? $sth->fetchColumn()
+		: $rowcount;
+}
+
+/**
+* Begin a transaction
+*
+* @return boolean TRUE on success or FALSE on failure
+*/
+public function beginTransaction()
+{
+	return $this->dbh->exec('BEGIN TRANSACTION') && ($this->transaction = TRUE);
+}
+
+/**
+* Commit a transaction
+*
+* @return boolean TRUE on success or FALSE on failure
+*/
+public function commit()
+{
+	return $this->dbh->exec('COMMIT TRANSACTION') && (($this->transaction = FALSE) || TRUE);
+}
+
+/**
+* Rollback the current transaction
+*
+* @return boolean TRUE on success or FALSE on failure
+*/
+public function rollBack()
+{
+	return $this->dbh->exec('ROLLBACK TRANSACTION') && (($this->transaction = FALSE) || TRUE);
+}
 
 /*=======================================================*/
 }
