49229de6f1ccb3de084aba78c9d877a7c213cd07
John Yau <jyau@pmi.edu>
2021-06-23 12:28:05 -0700

    Add pgsql statement driver

--- library/Db/Statement/Driver/Pdo/Pdo/Mysql.php
+++ library/Db/Statement/Driver/Pdo/Pdo/Mysql.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
 * Yau Tools
 *
---
+++ library/Db/Statement/Driver/Pdo/Pdo/Pgsql.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+* Yau Tools
+*
+* @author   John Yau
+* @category Yau
+* @package  Yau_Db
+*/
+
+namespace Yau\Db\Statement\Driver\Pdo\Pdo;
+
+use Yau\Db\Statement\Driver\Pdo\Pdo;
+
+/**
+* Statement object for use with a PDO Pgsql connection
+*
+* @author   John Yau
+* @category Yau
+* @package  Yau_Db
+*/
+class Pgsql extends Pdo
+{
+/*=======================================================*/
+
+/*=======================================================*/
+}
