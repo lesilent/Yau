56c6331bcd6ee5604ac993073f23b6fa0395a839
John Yau <jyau@pmi.edu>
2025-09-04 09:42:53 -0700

    Minor change to temp_filename to make prefix optional

--- src/Functions/temp_filename.php
+++ src/Functions/temp_filename.php
@@ -5,15 +5,15 @@ namespace Yau\Functions;
 /**
  * Create a file with a unique filename and register function to remove upon shutdown
  *
- * @param string $prefix the prefix name
+ * @param string $prefix the optional prefix name
  * @param string $directory optional directory if other than path used for temporary files
  * @return string|false
  */
-function temp_filename(string $prefix, ?string $directory = null)
+function temp_filename(string $prefix = 'yau', ?string $directory = null)
 {
 	if (($fname = tempnam(empty($directory) ? sys_get_temp_dir() : $directory, $prefix)) !== false)
 	{
-		register_shutdown_function(fn() => file_exists($fname) ? unlink($fname) : false);
+		register_shutdown_function(fn() => file_exists($fname) && unlink($fname));
 	}
 	return $fname;
 }
